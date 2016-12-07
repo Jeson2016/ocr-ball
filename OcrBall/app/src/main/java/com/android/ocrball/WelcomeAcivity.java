7ef2155b26bb8e4b30e814d284c3c56fbbe7617e
jie.liu <jie.liu@ck-telecom.com>
2016-12-07 10:23:14 +0800

    Add new Android Studio project for OCR

---
+++ OcrBall/app/src/main/java/com/android/ocrball/WelcomeAcivity.java
@@ -0,0 +1,13 @@
+package com.android.ocrball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_acivity);
+    }
+}
